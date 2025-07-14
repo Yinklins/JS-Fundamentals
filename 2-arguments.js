a62dcc880d7cc3c0bd4b2211ed2b8626d25a8f48
Olayinka Adeyeye <93340269+Yinklins@users.noreply.github.com>
2025-07-14 01:55:56 +0100

    modified 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,8 +3,8 @@ const args = ['node', 'hello']
 if (args.length === 0){
      console.log("No argument")
 }else if (args.length === 1){
-    console.log("Argument found")
-}else {console.log("Arguments found")};
+    console.log("Best Argument found")
+}else {console.log("Best Arguments found")};
 
     args.forEach ((arg, index) => {
         console.log(`Argument ${index+1}: ${arg}`);
