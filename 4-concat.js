7b39848e3f85f7a820340969ca68b16d5243bbc6
Olayinka Adeyeye <93340269+Yinklins@users.noreply.github.com>
2025-07-14 04:38:32 +0100

    correct 4-concat

--- 4-concat.js
+++ 4-concat.js
@@ -1,8 +1,8 @@
-const speak = function(name, index){
+const speak = function(name, index,){
     console.log(`${name} is ${index}`);
 }
 
 
 speak("Python","fun");
-speak("HBTN","Undefined");
+speak("HBTN","undefined");
 speak("undefined", "undefined");
