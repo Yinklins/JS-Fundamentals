ccccd99ce7912876f418ca1e534b9fb9865cd6ff
Olayinka Adeyeye <93340269+Yinklins@users.noreply.github.com>
2025-07-14 03:06:09 +0100

    file 2-arguments.js updated

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,11 +1,7 @@
 const args = ['node', 'hello', "oyin"]
 
-// if (args.length === 0){
-//      console.log("No argument")
-// }else if (args.length === 1){
-//     console.log("Best Argument found")
-// }else {console.log("Best Arguments found")};
-
-    args.forEach ((arg, index) => {
-        console.log(`Argument ${index+1}: ${arg}`);
-    });
+if (args.length === 0){
+     console.log("No argument")
+}else if (args.length === 1){
+    console.log("Best Argument found")
+}else {console.log("Best Arguments found")};
