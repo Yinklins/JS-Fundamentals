a7e91056938e54a5c41c55bae2015e16bc4aacc2
Olayinka Adeyeye <93340269+Yinklins@users.noreply.github.com>
2025-07-14 01:24:23 +0100

    Add 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.args.slice("2")
+
+if (args.length === 0){
+    console.log("No Argument found");
+}else if (args.length === 1){
+    console.log("Argument found");
+}else{
+    console.log("Arguments found");
+}
