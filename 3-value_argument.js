9ac337bd0d564246451d75daac5326371aee995f
Olayinka Adeyeye <93340269+Yinklins@users.noreply.github.com>
2025-07-14 10:23:13 +0100

    modify 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,9 @@
-const args = ['Hello', 'world']
+const args = process.argv.slice(2);
 
 if (args[0] === undefined){
     console.log("No argument");
 } else if (args[1]===undefined){
-    console.log("only one argumetn:" + args[0]);
+    console.log(args[0]);
 }else {
     console.log("First argument:" + args[0]);
 }
