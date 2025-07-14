05fba3fd4088a1e134f93d6bf516861b1208ef27
Olayinka Adeyeye <93340269+Yinklins@users.noreply.github.com>
2025-07-14 02:18:03 +0100

    modified 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,6 +2,8 @@ const args = ['Hello', 'world']
 
 if (args[0] === undefined){
     console.log("No argument");
-} else {
-    console.log(args[0]);
+} else if (args[1]===undefined){
+    console.log("only one argumetn:" + args[0]);
+}else {
+    console.log("First argument:" + args[0]);
 }
